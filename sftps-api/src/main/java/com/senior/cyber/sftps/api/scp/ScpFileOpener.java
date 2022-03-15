79554b5d4d60eeef47b499b6c6cd612538a5b00a
Senior Cyber <senior.cyber@gmail.com>
2022-03-15 22:05:10 +0700

    more

--- sftps-api/src/main/java/com/senior/cyber/sftps/api/scp/ScpFileOpener.java
+++ sftps-api/src/main/java/com/senior/cyber/sftps/api/scp/ScpFileOpener.java
@@ -27,7 +27,7 @@ public class ScpFileOpener extends DefaultScpFileOpener {
         OutputStream origin = super.openWrite(session, file, size, permissions, options);
         if (user.getFakeDictionary() != null && user.getOriginDictionary() != null) {
             LOGGER.info("openWrite with DRM");
-            return new DrmOutputStream(origin, user.getOriginDictionary());
+            return new DrmOutputStream(origin, user.getFakeDictionary());
         } else {
             LOGGER.info("openWrite");
             return origin;
@@ -41,7 +41,7 @@ public class ScpFileOpener extends DefaultScpFileOpener {
         InputStream origin = super.openRead(session, file, size, permissions, options);
         if (user.getFakeDictionary() != null && user.getOriginDictionary() != null) {
             LOGGER.info("openRead with DRM");
-            return new DrmInputStream(origin, user.getFakeDictionary());
+            return new DrmInputStream(origin, user.getOriginDictionary());
         } else {
             LOGGER.info("openRead");
             return origin;
