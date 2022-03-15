df504293cad25f0a0de0595cc996afbe6f110da3
Senior Cyber <senior.cyber@gmail.com>
2022-03-15 21:59:09 +0700

    more

--- sftps-api/src/main/java/com/senior/cyber/sftps/api/scp/ScpFileOpener.java
+++ sftps-api/src/main/java/com/senior/cyber/sftps/api/scp/ScpFileOpener.java
@@ -5,6 +5,8 @@ import com.senior.cyber.sftps.api.ftp.DrmInputStream;
 import com.senior.cyber.sftps.api.ftp.DrmOutputStream;
 import org.apache.sshd.common.session.Session;
 import org.apache.sshd.scp.common.helpers.DefaultScpFileOpener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,14 +18,19 @@ import java.util.Set;
 
 public class ScpFileOpener extends DefaultScpFileOpener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScpFileOpener.class);
+
     @Override
     public OutputStream openWrite(Session session, Path file, long size, Set<PosixFilePermission> permissions, OpenOption... options) throws IOException {
         org.apache.sshd.server.session.ServerSession serverSession = (org.apache.sshd.server.session.ServerSession) session;
         SftpSUser user = (SftpSUser) serverSession.getProperties().get(SftpSUser.USER_SESSION);
+        OutputStream origin = super.openWrite(session, file, size, permissions, options);
         if (user.getFakeDictionary() != null && user.getOriginDictionary() != null) {
-            return new DrmOutputStream(super.openWrite(session, file, size, permissions, options), user.getOriginDictionary());
+            LOGGER.info("openWrite with DRM");
+            return new DrmOutputStream(origin, user.getOriginDictionary());
         } else {
-            return super.openWrite(session, file, size, permissions, options);
+            LOGGER.info("openWrite");
+            return origin;
         }
     }
 
@@ -31,10 +38,13 @@ public class ScpFileOpener extends DefaultScpFileOpener {
     public InputStream openRead(Session session, Path file, long size, Set<PosixFilePermission> permissions, OpenOption... options) throws IOException {
         org.apache.sshd.server.session.ServerSession serverSession = (org.apache.sshd.server.session.ServerSession) session;
         SftpSUser user = (SftpSUser) serverSession.getProperties().get(SftpSUser.USER_SESSION);
+        InputStream origin = super.openRead(session, file, size, permissions, options);
         if (user.getFakeDictionary() != null && user.getOriginDictionary() != null) {
-            return new DrmInputStream(super.openRead(session, file, size, permissions, options), user.getFakeDictionary());
+            LOGGER.info("openRead with DRM");
+            return new DrmInputStream(origin, user.getFakeDictionary());
         } else {
-            return super.openRead(session, file, size, permissions, options);
+            LOGGER.info("openRead");
+            return origin;
         }
     }
 
